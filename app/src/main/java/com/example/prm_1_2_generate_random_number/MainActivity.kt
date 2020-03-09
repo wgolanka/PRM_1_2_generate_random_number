87272e02b9df853dfa730f53de11bb69a22800f8
wgolanka <wiktoria.golanka@gmail.com>
2020-03-09 21:35:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/prm_1_2_generate_random_number/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prm_1_2_generate_random_number
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
